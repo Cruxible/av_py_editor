d679d555fdfd5331d59f0c4eccc727a3cbd59c4a
Jon Cruxible <tagrous87@gmail.com>
2023-11-02 09:18:59 -0400

    main_editor.py

--- main_editor.py
+++ main_editor.py
@@ -9,7 +9,7 @@ from moviepy.editor import *
 from moviepy.editor import VideoFileClip
 from moviepy.video.io.VideoFileClip import VideoFileClip
 import sys
-import pytube
+import subprocess
 
 class main_logo:
 	def logo():
@@ -51,20 +51,20 @@ class MySexyVariables:
 				]
 
 class Input:
-    @staticmethod
-    def get_string_input():
-        console = Console()
-        return console.input(" [bright_black]_______________________________________________[/bright_black]" + "[bright_black]\n  __ [/bright_black]" + "[red1]" + MySexyVariables.curdir + " [/red1]" + "[bright_black]\n (__[/bright_black]" + "[purple]" + MySexyVariables.user + "[/purple]" + "[bright_black]__: [/bright_black]")
+	@staticmethod
+	def get_string_input():
+		console = Console()
+		return console.input(" [bright_black]_______________________________________________[/bright_black]" + "[bright_black]\n  __ [/bright_black]" + "[red1]" + MySexyVariables.curdir + " [/red1]" + "[bright_black]\n (__[/bright_black]" + "[purple]" + MySexyVariables.user + "[/purple]" + "[bright_black]__: [/bright_black]")
     
-    @staticmethod
-    def get_integer_input():
-        console = Console()
-        return int(console.input(" [bright_black]_______________________________________________[/bright_black]" + "[bright_black]\n  __ [/bright_black]" + "[red1]" + MySexyVariables.curdir + " [/red1]" + "[bright_black]\n (__[/bright_black]" + "[purple]" + MySexyVariables.user + "[/purple]" + "[bright_black]__: [/bright_black]"))
+	@staticmethod
+	def get_integer_input():
+		console = Console()
+		return int(console.input(" [bright_black]_______________________________________________[/bright_black]" + "[bright_black]\n  __ [/bright_black]" + "[red1]" + MySexyVariables.curdir + " [/red1]" + "[bright_black]\n (__[/bright_black]" + "[purple]" + MySexyVariables.user + "[/purple]" + "[bright_black]__: [/bright_black]"))
 
-    @staticmethod
-    def get_float_input():
-        console = Console()
-        return float(console.input(" [bright_black]_______________________________________________[/bright_black]" + "[bright_black]\n  __ [/bright_black]" + "[red1]" + MySexyVariables.curdir + " [/red1]" + "[bright_black]\n (__[/bright_black]" + "[purple]" + MySexyVariables.user + "[/purple]" + "[bright_black]__: [/bright_black]"))
+	@staticmethod
+	def get_float_input():
+		console = Console()
+		return float(console.input(" [bright_black]_______________________________________________[/bright_black]" + "[bright_black]\n  __ [/bright_black]" + "[red1]" + MySexyVariables.curdir + " [/red1]" + "[bright_black]\n (__[/bright_black]" + "[purple]" + MySexyVariables.user + "[/purple]" + "[bright_black]__: [/bright_black]"))
 
 class calls:
 	@staticmethod
@@ -75,58 +75,82 @@ class calls:
 		print(" ", tree)
 
 class list_dirs:
-    @staticmethod
-    def vid_list():
-        os.chdir(MySexyVariables.video_dir)
-        tree = Tree("[bright_black]" + MySexyVariables.video_dir, guide_style="purple")
-        for i in MySexyVariables.vid_list:
-            tree.add("[bright_black]" + str(i))
-        print(" ", tree)
+	@staticmethod
+	def vid_list():
+		os.chdir(MySexyVariables.video_dir)
+		tree = Tree("[bright_black]" + MySexyVariables.video_dir, guide_style="purple")
+		for i in MySexyVariables.vid_list:
+			tree.add("[bright_black]" + str(i))
+		print(" ", tree)
 
-    @staticmethod
-    def music_list():
-        os.chdir(MySexyVariables.audio_dir)
-        tree = Tree("[bright_black]" + MySexyVariables.audio_dir, guide_style="purple")
-        for i in MySexyVariables.audio_list:
-            tree.add("[bright_black]" + str(i))
-        print(" ", tree)
+	@staticmethod
+	def music_list():
+		os.chdir(MySexyVariables.audio_dir)
+		tree = Tree("[bright_black]" + MySexyVariables.audio_dir, guide_style="purple")
+		for i in MySexyVariables.audio_list:
+			tree.add("[bright_black]" + str(i))
+		print(" ", tree)
 
-    @staticmethod
-    def desktop_list():
-        os.chdir(MySexyVariables.desktop_dir)
-        tree = Tree("[bright_black]" + MySexyVariables.desktop_dir, guide_style="purple")
-        for i in MySexyVariables.desktop_list:
-            tree.add("[bright_black]" + str(i))
-        print(" ", tree)
+	@staticmethod
+	def desktop_list():
+		os.chdir(MySexyVariables.desktop_dir)
+		tree = Tree("[bright_black]" + MySexyVariables.desktop_dir, guide_style="purple")
+		for i in MySexyVariables.desktop_list:
+			tree.add("[bright_black]" + str(i))
+		print(" ", tree)
 
-    @staticmethod
-    def picture_list():
-        os.chdir(MySexyVariables.pics_dir)
-        tree = Tree("[bright_black]" + MySexyVariables.pics_dir, guide_style="purple")
-        for i in MySexyVariables.pics_list:
-            tree.add("[bright_black]" + str(i))
-        print(" ", tree)
+	@staticmethod
+	def picture_list():
+		os.chdir(MySexyVariables.pics_dir)
+		tree = Tree("[bright_black]" + MySexyVariables.pics_dir, guide_style="purple")
+		for i in MySexyVariables.pics_list:
+			tree.add("[bright_black]" + str(i))
+		print(" ", tree)
 
 class main_functions:
 	def download_video():
 		def download_call():
-			print(" [bright_black]Please enter a link.[/bright_black]")
-			url = Input.get_string_input()
-			youtube = pytube.YouTube(url)
-			video = youtube.streams.filter(res="720p").first()
-			video.download()
+			print(' How would you like to download?')
+			list_choice = ['mp3', 'best video', 'choose format']
+			tree = Tree("[bright_black]" + MySexyVariables.pics_dir, guide_style="purple")
+			for i in list_choice:
+				tree.add("[bright_black]" + str(i))
+			print(" ", tree)
+			video_format = Input.get_string_input()
+			if video_format == 'mp3':
+				print(' Please enter a link')
+				url = Input.get_string_input()
+				mp3_command = f"yt-dlp -x --audio-format mp3 {url}"
+				os.system(mp3_command)
+			elif video_format == 'best video':
+				print(' Please enter a link')
+				url = Input.get_string_input()
+				best_video_command = f'yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]" {url}'
+				os.system(best_video_command)
+			elif video_format == 'choose format':
+				print(' Please enter a link')
+				url = Input.get_string_input()
+				format_command = f"yt-dlp -F {url}"
+				output = subprocess.check_output(format_command, shell=True).decode()
+				print(output)
+				format = input("Enter the format code: ")
+				format_code_command = f"yt-dlp -f {format} {url}"
+				os.system(format_code_command)
+			elif video_format == 'exit':
+				sys.exit()
+
 		print(HonerableMentions.save_vid_where)
 		directory = Input.get_string_input()
 		if directory == 'Desktop':
 			os.chdir(MySexyVariables.desktop_dir)
 			download_call()
-		if directory == 'Videos':
+		elif directory == 'Videos':
 			os.chdir(MySexyVariables.video_dir)
 			download_call()
-		if directory == 'droid':
+		elif directory == 'droid':
 			os.chdir(MySexyVariables.sd_video_dir)
 			download_call()
-		if "exit" == directory:
+		elif "exit" == directory:
 			sys.exit()
 		
 
@@ -153,17 +177,17 @@ class main_functions:
 					print(HonerableMentions.new_filename)
 					new_file = Input.get_string_input()
 					final.write_videofile(f"{new_file}")
-				if directory == 'Videos':
+				elif directory == 'Videos':
 					os.chdir(MySexyVariables.video_dir)
 					print(HonerableMentions.new_filename)
 					new_file = Input.get_string_input()
 					final.write_videofile(f"{new_file}")
-				if directory == 'droid':
+				elif directory == 'droid':
 					os.chdir(MySexyVariables.sd_video_dir)
 					print(HonerableMentions.new_filename)
 					new_file = Input.get_string_input()
 					final.write_videofile(f"{new_file}")
-				if "exit" == directory:
+				elif "exit" == directory:
 					sys.exit()
 			if "exit" == audio_file:
 				sys.exit()
@@ -188,13 +212,13 @@ class main_functions:
 			if directory == 'Desktop':
 				os.chdir(MySexyVariables.desktop_dir)
 				new_audio.write_audiofile(output_file_path)
-			if directory == 'Music':
+			elif directory == 'Music':
 				os.chdir(MySexyVariables.audio_dir)
 				new_audio.write_audiofile(output_file_path)
-			if directory == 'droid':
+			elif directory == 'droid':
 				os.chdir(MySexyVariables.sd_Music_dir)
 				new_audio.write_audiofile(output_file_path)
-			if "exit" == directory:
+			elif "exit" == directory:
 				sys.exit()
 		if "exit" == filename:
 			sys.exit()
@@ -219,13 +243,13 @@ class main_functions:
 			if directory == 'Desktop':
 				os.chdir(MySexyVariables.desktop_dir)
 				clip.write_videofile(new_file)
-			if directory == 'Videos':
+			elif directory == 'Videos':
 				os.chdir(MySexyVariables.video_dir)
 				clip.write_videofile(new_file)
-			if directory == 'droid':
+			elif directory == 'droid':
 				os.chdir(MySexyVariables.sd_video_dir)
 				clip.write_videofile(new_file)
-			if "exit" == directory:
+			elif "exit" == directory:
 				sys.exit()
 		if "exit" == filename:
 			sys.exit()
@@ -247,13 +271,13 @@ class main_functions:
 			if directory == 'Desktop':
 				os.chdir(MySexyVariables.desktop_dir)
 				clip.write_videofile(new_vid_name)
-			if directory == 'Videos':
+			elif directory == 'Videos':
 				os.chdir(MySexyVariables.video_dir)
 				clip.write_videofile(new_vid_name)
-			if directory == 'droid':
+			elif directory == 'droid':
 				os.chdir(MySexyVariables.sd_video_dir)
 				clip.write_videofile(new_vid_name)
-			if "exit" == directory:
+			elif "exit" == directory:
 				sys.exit()
 		if "exit" == vid_name:
 			sys.exit()
@@ -272,13 +296,13 @@ class main_functions:
 			if directory == 'Desktop':
 				os.chdir(MySexyVariables.desktop_dir)
 				audio.write_audiofile(new_filename)
-			if directory == 'Music':
+			elif directory == 'Music':
 				os.chdir(MySexyVariables.audio_dir)
 				audio.write_audiofile(new_filename)
-			if directory == 'droid':
+			elif directory == 'droid':
 				os.chdir(MySexyVariables.sd_Music_dir)
 				audio.write_audiofile(new_filename)
-			if "exit" == directory:
+			elif "exit" == directory:
 				sys.exit()
 		if "exit" == filename:
 			sys.exit()
@@ -300,13 +324,13 @@ class main_functions:
 				if directory == 'Desktop':
 					os.chdir(MySexyVariables.desktop_dir)
 					final_audio.write_audiofile(new_file)
-				if directory == 'Music':
+				elif directory == 'Music':
 					os.chdir(MySexyVariables.audio_dir)
 					final_audio.write_audiofile(new_file)
-				if directory == 'droid':
+				elif directory == 'droid':
 					os.chdir(MySexyVariables.sd_Music_dir)
 					final_audio.write_audiofile(new_file)
-				if "exit" == directory:
+				elif "exit" == directory:
 					sys.exit()
 			if "exit" == second_file:
 				sys.exit()
@@ -331,13 +355,13 @@ class main_functions:
 				if directory == 'Desktop':
 					os.chdir(MySexyVariables.desktop_dir)
 					final_video.write_videofile(new_file)
-				if directory == 'Videos':
+				elif directory == 'Videos':
 					os.chdir(MySexyVariables.video_dir)
 					final_video.write_videofile(new_file)
-				if directory == 'droid':
+				elif directory == 'droid':
 					os.chdir(MySexyVariables.sd_video_dir)
 					final_video.write_videofile(new_file)
-				if "exit" == directory:
+				elif "exit" == directory:
 					sys.exit()
 			if "exit" == second_file:
 				sys.exit()
@@ -357,44 +381,44 @@ class Main:
 					main_logo.logo()
 					os.system('clear')
 					Main.main()
-				if command == MySexyVariables.calls_list[1]:
+				elif command == MySexyVariables.calls_list[1]:
 					main_functions.cut_vid()
 					main_logo.logo()
 					os.system('clear')
 					Main.main()
-				if command == MySexyVariables.calls_list[2]:
+				elif command == MySexyVariables.calls_list[2]:
 					main_functions.cut_mp3()
 					main_logo.logo()
 					os.system('clear')
 					Main.main()
-				if command == MySexyVariables.calls_list[3]:
+				elif command == MySexyVariables.calls_list[3]:
 					main_functions.volume_adjust()
 					main_logo.logo()
 					os.system('clear')
 					Main.main()
-				if command == MySexyVariables.calls_list[4]:
+				elif command == MySexyVariables.calls_list[4]:
 					main_functions.over_lay()
 					main_logo.logo()
 					os.system('clear')
 					Main.main()
-				if command == MySexyVariables.calls_list[5]:
+				elif command == MySexyVariables.calls_list[5]:
 					main_functions.ext_audio()
 					main_logo.logo()
 					os.system('clear')
 					Main.main()
-				if command == MySexyVariables.calls_list[6]:
+				elif command == MySexyVariables.calls_list[6]:
 					main_functions.concatenate_mp3()
 					main_logo.logo()
 					os.system('clear')
 					Main.main()
-				if command == MySexyVariables.calls_list[7]:
+				elif command == MySexyVariables.calls_list[7]:
 					main_functions.concatenate_videos()
 					main_logo.logo()
 					os.system('clear')
 					Main.main()
-				if command == MySexyVariables.calls_list[8]:
+				elif command == MySexyVariables.calls_list[8]:
 					sys.exit()
-				if command == 'list':
+				elif command == 'list':
 					list_dirs.vid_list()
 					list_dirs.music_list()
 					list_dirs.desktop_list()
